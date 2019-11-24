816566d787118ca64f6cc0671d9822d309718bd6
vinod <vinod@cse.iitb.ac.in>
2019-11-24 11:32:00 +0530

    trainset split into train and test set

--- untitled.py
+++ untitled.py
@@ -9,10 +9,14 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 
+def split_dataset(X,Y):
+	return train_test_split(X, Y, test_size = 0.2,random_state = 0)
 
-def preprocessData(data,train=False):
+
+def preprocessData(data):
 	data['Item_Fat_Content'].replace(['low fat','LF','reg'],['Low Fat','Low Fat','Regular'],inplace = True)
 	data['num_years'] = data['Outlet_Establishment_Year'].apply(lambda x: 2013 - x)
 	data['Item_Weight'].fillna(data['Item_Weight'].mean(),inplace = True)
@@ -20,26 +24,18 @@ def preprocessData(data,train=False):
 	col = ['Item_Fat_Content', 'Item_Type', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type']
 	data_temp = pd.get_dummies(data, columns = col, drop_first = True)
 	feat_cols = ['Item_Weight', 'Item_Visibility', 'Item_MRP', 'num_years','Item_Fat_Content_Regular', 'Item_Type_Breads', 'Item_Type_Breakfast','Item_Type_Canned', 'Item_Type_Dairy', 'Item_Type_Frozen Foods','Item_Type_Fruits and Vegetables', 'Item_Type_Hard Drinks','Item_Type_Health and Hygiene', 'Item_Type_Household', 'Item_Type_Meat','Item_Type_Others', 'Item_Type_Seafood', 'Item_Type_Snack Foods','Item_Type_Soft Drinks', 'Item_Type_Starchy Foods','Outlet_Size_Medium', 'Outlet_Size_Small','Outlet_Location_Type_Tier 2', 'Outlet_Location_Type_Tier 3','Outlet_Type_Supermarket Type1', 'Outlet_Type_Supermarket Type2','Outlet_Type_Supermarket Type3']
-	if(train):
-		X = data_temp[feat_cols]
-		Y = data_temp['Item_Outlet_Sales'] 
-	else:
-		X =data_temp[feat_cols]
-		Y = None
+	X = data_temp[feat_cols]
+	Y = data_temp['Item_Outlet_Sales'] 
 	return X,Y
 
 def rmse(pred,Y_train):
 	return math.sqrt(mean_squared_error(Y_train,lr_pred))
 
 def find_accuracy(pred,y):
-	error=0
-	ysum=0
-	for i in range(len(pred)):
-		error+=abs(pred[i]-y[i])
-		ysum+=y[i]
-	return 100-(error/ysum)*100
 
-def linear_regression(X_train,Y_train,X_test):
+	return 100-(np.sum(np.absolute(np.subtract(pred,y)))/np.sum(y))*100
+
+def linear_regression(X_train,Y_train,X_test,Y_test):
 
 
 
@@ -47,16 +43,8 @@ def linear_regression(X_train,Y_train,X_test):
 	lr.fit(X_train, Y_train)
 
 
-	lr_pred = lr.predict(X_train)
-	# #lr_accuracy=0
-	# for i in range(len(lr_pred)):
-	# 	lr_accuracy+= (Y_train[i]- lr_pred[i])**2
-	# 	with open('prediction.csv', 'w') as writeFile:
-	# 	    writer = csv.writer(writeFile)
-	# 	    row=str(i)+" "+str(lr_pred[i])
-	# 	    writer.writerows(row)
-
-	lr_accuracy=find_accuracy(lr_pred,Y_train)	    
+	lr_pred = lr.predict(X_test)
+	lr_accuracy=find_accuracy(lr_pred,Y_test)	    
 	print(lr_accuracy)
 
 def neural_network(X_train,Y_train,X_test):
@@ -85,12 +73,10 @@ def random_forest(X,Y):
 def main():
 	
 	train_df = pd.read_csv('./dataset/Train.csv')
-	test_df = pd.read_csv('./dataset/Test.csv')
-
-	X_train,Y_train=preprocessData(train_df,True)
-	X_test,Y=preprocessData(test_df)
+	X_train,Y_train=preprocessData(train_df)
+	X_train, X_test, Y_train, Y_test=split_dataset(X_train,Y_train)
 	print(X_train.shape)
-	linear_regression(X_train,Y_train,X_test)
+	linear_regression(X_train,Y_train,X_test,Y_test)
 	#bagged_decision_tree(X_train,Y_train)
 
 main()
